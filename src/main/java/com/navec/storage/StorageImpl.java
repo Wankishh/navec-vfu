6cf58876dd8eddbc776ce161a6c6bae4758d7260
Ivan Velinov <wankishh@gmail.com>
2023-03-27 06:59:46 +0300

    move props to properties

--- src/main/java/com/navec/storage/StorageImpl.java
+++ src/main/java/com/navec/storage/StorageImpl.java
@@ -6,27 +6,39 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.InputStream;
 
 public class StorageImpl implements Storage {
-    private final AmazonS3 s3client;
+    private AmazonS3 s3client;
 
-    public StorageImpl() {
-        AWSCredentials credentials = new BasicAWSCredentials(
-                "AKIA2OLDO4BPHBYC6XWF",
-                "InXWMTxRAxmoLCoeiKHioGbnnIr7x9mdGAEi9vkV"
-        );
+    @Value("${config.storage_key}")
+    private String accessKey;
 
-        this.s3client = AmazonS3ClientBuilder
-                .standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(Regions.EU_WEST_2)
-                .build();
+    @Value("${config.storage_priv_key}")
+    private String privateKey;
+
+    private AmazonS3 getClient() {
+        if(this.s3client == null) {
+            AWSCredentials credentials = new BasicAWSCredentials(
+                    accessKey,
+                    privateKey
+            );
+
+            this.s3client = AmazonS3ClientBuilder
+                    .standard()
+                    .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                    .withRegion(Regions.EU_WEST_2)
+                    .build();
+        }
+
+        return this.s3client;
     }
 
     @Override
     public void putFile(String filename, InputStream stream) {
-        this.s3client.putObject("navecstorage", filename, stream, null);
+        String bucketName = "navecstorage";
+        this.getClient().putObject(bucketName, filename, stream, null);
     }
 }
